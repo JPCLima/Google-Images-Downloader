aea31ed28015fe37a3278fa0de4cc4eb6f11c5e0
Joao Lima <joaoplima@ua.pt>
2021-08-05 15:55:36 +0200

    implemented dowload images

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+google_img = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
+user_agent = u_agnt = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive',
+}
+
+SAVE_FOLDER = 'img_downloaded'
+
+
+def get_img_links(data, num_images):
+    search_url = google_img + 'q=' + data
+
+    response = requests.get(search_url, headers=user_agent)
+    html = response.text
+
+    b_soup = BeautifulSoup(html, 'html.parser')
+    results = b_soup.findAll('img', {'class': 'rg_i Q4LuWd'})
+
+    count = 0
+    img_links = []
+    for res in results:
+        try:
+            if (count < int(num_images)):
+                link = res['data-src']
+                img_links.append(link)
+                count = count + 1
+        except KeyError:
+            continue
+
+    return img_links
+
+
+def download_imgs(data, img_links):
+
+    for i, link in enumerate(img_links):
+        response = requests.get(link)
+        img_name = SAVE_FOLDER + '/' + data + '-' + str(i) + '.jpg'
+        with open(img_name, 'wb') as file:
+            file.write(response.content)
+
+
+def main():
+
+    data = 'cola-can'
+    num_images = 3
+
+    if not os.path.exists(SAVE_FOLDER):
+        os.mkdir(SAVE_FOLDER)
+
+    link_list = get_img_links(data, num_images)
+    download_imgs(data, link_list)
+
+
+if __name__ == "__main__":
+    main()
